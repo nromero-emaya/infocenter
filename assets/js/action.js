f060cb8d87f248b663612d5910a8c43c7f18e63a
jhuezo-emaya <124105186+jhuezo-emaya@users.noreply.github.com>
2023-03-27 09:58:44 -0600

    Add files via upload

---
+++ assets/js/action.js
@@ -0,0 +1,10 @@
+
+
+function expand(card) {
+            card.classList.toggle('profile--expanded');
+
+            // If card is not expanded after toggle, add 'unexpanded' class
+            if (!card.classList.contains('profile--expanded')) card.classList.toggle('profile--unexpanded');
+            // Else if card is expanded after toggle and still contains 'unexpanded' class, remove 'unexpanded'
+            else if (card.classList.contains('profile--expanded') && card.classList.contains('profile--unexpanded')) card.classList.toggle('profile--unexpanded');
+        }
